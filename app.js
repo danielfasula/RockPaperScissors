9a21866d44c088e48a93feaf0fd891923f0b0d70
danielfasula <danielfasula@yahoo.com>
2019-01-24 18:03:47 -0700

    ALERTS!

--- app.js
+++ app.js
@@ -4,27 +4,27 @@ function play(playerChoice) {
   let computerChoice = choices[Math.floor(Math.random() * 3)]
   if (playerChoice == "rock") {
     if (computerChoice == "rock") {
-      console.log("You Tie")
+      window.alert("You Tie")
     } else if (computerChoice == "paper") {
-      console.log("You Lose")
+      window.alert("You Lose")
     } else if (computerChoice == "scissors") {
-      console.log("You Win!")
+      window.alert("You Win!")
     }
   } else if (playerChoice == "paper") {
     if (computerChoice == "paper") {
-      console.log("You Tie")
+      window.alert("You Tie")
     } else if (computerChoice == "scissors") {
-      console.log("You Lose")
+      window.alert("You Lose")
     } else if (computerChoice == "rock") {
-      console.log("You Win!")
+      window.alert("You Win!")
     }
   } else if (playerChoice == "scissors") {
     if (computerChoice == "scissors") {
-      console.log("You Tie")
+      window.alert("You Tie")
     } else if (computerChoice == "rock") {
-      console.log("You Lose")
+      window.alert("You Lose")
     } else if (computerChoice == "paper") {
-      console.log("You Win!")
+      window.alert("You Win!")
     }
   }
 }
